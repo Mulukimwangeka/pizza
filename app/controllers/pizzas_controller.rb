00f9565679b83c61e0fe4ea7c3923c65c91f80be
mulukimwangeka <mwangekaruth@gmail.com>
2022-11-29 15:41:33 +0300

    fakers created

--- app/controllers/pizzas_controller.rb
+++ app/controllers/pizzas_controller.rb
@@ -1,2 +1,36 @@
 class PizzasController < ApplicationController
+    def index
+        @pizzas = Pizza.all
+        render json: @pizzas
+    end
+
+    def show
+        @pizza = Pizza.find(params[:id])
+        render json: @pizza
+    end
+
+    def create
+        @pizza = Pizza.new(pizza_params)
+        render json: @pizza
+    end
+
+    def update
+        @pizza = Pizza.find(params[:id])
+        @pizza.update(pizza_params)
+        render json: @pizza
+
+    end
+
+    def destroy
+        @pizza = Pizza.find(params[:id])
+        @pizza.destroy
+        render json: @pizza
+    end
+
+    private
+    
+    def pizza_params
+        params.require(:pizza).permit(:name, :ingredients)
+    end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,31 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+Restaurant.destroy_all
+Pizza.destroy_all
+RestaurantPizza.destroy_all
+
+#faker for pizza
+30.times do
+    Pizza.create(
+        name: Faker::Food.ingredient,
+        #has 3 ingredients in an array
+        ingredients: [Faker::Food.ingredient, Faker::Food.ingredient, Faker::Food.ingredient],
+
+    )
+
+end
+
+#faker for restaurant
+30.times do
+    Restaurant.create(
+        name: Faker::Restaurant.name,
+        address: Faker::Address.street_address
+    )
+end
+
+#faker for restaurant_pizza
+
+30.times do
+    RestaurantPizza.create(
+        restaurant_id:Restaurant.all.sample.id,
+        pizza_id: Pizza.all.sample.id
+    )
+end
